ef28246c6b8da7554fd921ee47b03cb1d99d22e0
Dexight <tupikin@sfedu.ru>
2025-05-20 23:54:27 +0300

    modern settings

--- HeartlessMaze/Assets/Scripts/SceneScripts/settings.cs
+++ HeartlessMaze/Assets/Scripts/SceneScripts/settings.cs
@@ -1,22 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class settings : MonoBehaviour
 {
+    private MicrophoneListener microphoneListener;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] Image audioBackgorund;
+    [SerializeField] Image thresholdBackground;
+    [SerializeField] Image sensibilityBackground;
 
     [SerializeField] private PauseMenu pauseMenu;
     [SerializeField] private PlayerMovement playerMovement;
-    
+
+    [SerializeField] private TMP_Text thresholdText;
+    [SerializeField] private TMP_Text sensibilityText;
+
     public GameObject[] arrows;
+    [SerializeField] private float curThreshold;
+    [SerializeField] private int curSensibility;
     public int currentButtonIndex = 0;
+
+    private void Start()
+    {
+        microphoneListener = MicrophoneListener.Instance;
+
+        curThreshold = Mathf.RoundToInt(microphoneListener.threshold * 10f);
+        curSensibility = Mathf.RoundToInt(microphoneListener.sensibility / 10f);
+
+        thresholdText.text = (curThreshold/10f).ToString();
+        sensibilityText.text = curSensibility.ToString();
+
+        thresholdBackground.fillAmount = curThreshold/30f;
+        sensibilityBackground.fillAmount = curSensibility/20f;
+    }
 
     public void nextButton()
     {
         arrows[currentButtonIndex].gameObject.SetActive(false);
         currentButtonIndex++;
-        if (currentButtonIndex == 2)
+        if (currentButtonIndex == 4)
         {
             currentButtonIndex = 0;
         }
@@ -29,14 +51,14 @@ public class settings : MonoBehaviour
         currentButtonIndex--;
         if (currentButtonIndex < 0)
         {
-            currentButtonIndex = 1;
+            currentButtonIndex = 3;
         }
         arrows[currentButtonIndex].gameObject.SetActive(true);
     }
 
     public void pressButton()
     {
-        if (currentButtonIndex == 1)
+        if (currentButtonIndex == 3)
         {
             playerMovement.pressEscape();
         }
@@ -44,13 +66,61 @@ public class settings : MonoBehaviour
 
     public void audioChangeLeft()
     {
-        audioSource.volume -= 0.1f;
-        audioBackgorund.fillAmount = audioSource.volume;
+        switch (currentButtonIndex)
+        {
+            case 0://audio
+                audioSource.volume -= 0.1f;
+                audioBackgorund.fillAmount = audioSource.volume; 
+                break;
+            case 1://threshold
+                if (curThreshold > 1)
+                {
+                    curThreshold -= 1f;
+                    microphoneListener.threshold = curThreshold / 10f;
+                    thresholdText.text = (curThreshold / 10f).ToString();
+                    thresholdBackground.fillAmount = curThreshold / 30f;
+                }
+                break;
+            case 2://sensibility
+                if (curSensibility > 1)
+                {
+                    curSensibility -= 1;
+                    microphoneListener.sensibility = curSensibility * 10;
+                    sensibilityText.text = curSensibility.ToString();
+                    sensibilityBackground.fillAmount = curSensibility / 20f;
+                }
+                break;
+            default: break;
+        }
     }
 
     public void audioChangeRight()
     {
-        audioSource.volume += 0.1f;
-        audioBackgorund.fillAmount = audioSource.volume;
+        switch (currentButtonIndex)
+        {
+            case 0://audio
+                audioSource.volume += 0.1f;
+                audioBackgorund.fillAmount = audioSource.volume;
+                break;
+            case 1://threshold
+                if (curThreshold < 30)
+                {
+                    curThreshold += 1f;
+                    microphoneListener.threshold = curThreshold / 10f;
+                    thresholdText.text = (curThreshold / 10f).ToString();
+                    thresholdBackground.fillAmount = curThreshold / 30f;
+                }
+                break;
+            case 2://sensibility
+                if (curSensibility < 20)
+                {
+                    curSensibility += 1;
+                    microphoneListener.sensibility = curSensibility * 10;
+                    sensibilityText.text = curSensibility.ToString();
+                    sensibilityBackground.fillAmount = curSensibility / 20f;
+                }
+                break;
+            default: break;
+        }
     }
 }
